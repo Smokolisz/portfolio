50884eea2722071772c054e92d31f89a33e72705
Adam Czwordon <adam.czwordon@propertyboss.com>
2023-04-09 21:36:53 +0200

    add purgecss

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
-'use strict'
+'use strict';
 
-const path = require('path')
-const autoprefixer = require('autoprefixer')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin');
+const glob = require('glob-all');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   mode: 'production',
@@ -16,6 +18,7 @@ module.exports = {
   },
   optimization: {
     usedExports: true, // <- remove unused function
+    minimizer: [new UglifyJsPlugin()],
   },
   devServer: {
     static: path.resolve(__dirname, 'dist'),
@@ -23,7 +26,16 @@ module.exports = {
     hot: true
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: './index.php' })
+    new HtmlWebpackPlugin({ template: './index.php' }),
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
+    new PurgeCSSPlugin({
+      paths: glob.sync([
+        path.join(__dirname, '**/*.{html,php}'),
+        path.join(__dirname, 'src/**/*.js'),
+        path.join(__dirname, 'node_modules/bootstrap/**/*.js'),
+        path.join(__dirname, 'node_modules/bootstrap/**/*.css'),
+      ])
+    })
   ],
   module: {
     rules: [
@@ -53,11 +65,5 @@ module.exports = {
         ]
       }
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: './index.php' }),
-    new MiniCssExtractPlugin({
-      filename: 'style.css'
-    })
-  ]
-}
+  }
+};
